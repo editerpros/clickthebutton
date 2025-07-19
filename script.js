163d1d029f3ac67db9bb9614168e7247d97ff2fc
EditerPro <advaitchithira@gmail.com>
2025-07-19 20:54:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function downloadGame() {
-  window.location.href = "https://mega.nz/file/xgBhmDCR#L_ZYh04AmaUvMBrsuP4OhdtMzg-oT-2D7qgxCTzzbBU"; // Replace with your actual file URL
+function toggleDropdown() {
+  const dropdown = document.getElementById("download-dropdown");
+  dropdown.classList.toggle("hidden");
 }
